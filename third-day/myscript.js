711c457235cb3346a019adf1b89b3e1a50b813dd
John H. Moreira <98336461+JHsully@users.noreply.github.com>
2022-08-02 23:34:24 -0300

    Atualizando o terceiro desafio
    Correção de Bugs e edição da função no fluxo do Front-End

--- third-day/myscript.js
+++ third-day/myscript.js
@@ -2,47 +2,123 @@
 
         let controle = 1;
         let controle2 = 1;
-        let controle3 = 1;
+        let controle3 = 2;
 
-        //tamanho das duas listas deverá ser igual para que a função verificaRetorno não de erro.
+        let carreira = [];    
+        let retornoDoUsuario;
+        let retornoFluxoFront;
+        let retornoFluxoBack;
+        let tecnologiasAprendidas = [];
+
+          //tamanho das listas semelhantes deverá ser igual para que a função que verifica o retorno não de erro.
         let respostasParaFront = ['front','front-end','frontend']
         let respostasParaBack = ['back','back-end','backend',]
-
+        let respostasFluxoFront1 = ['react','rect','ract']
+        let respostasFluxoFront2 = ['vue','vuue','vrue']
+        let respostasFluxoBack = ['c#','c','java','jaava','']
+        let respostasPositivas = ['sim','ok','pode ser','quero',]
+        let respostasNegativas = ['nao','não','nao quero','nao desejo',]
 
         function verificaRetorno (retorno,lista1,lista2){
 
-            for(i = 0; i < respostasParaBack.length; i++){
-                if(respostasParaFront[i] == retornoDoUsuario){
-                        return true;
+            for(i = 0; i < lista1.length; i++){
+                if(lista1[i] == retorno){
+                    return true;
                 }  
-                if(respostasParaBack[i] == retornoDoUsuario){
-                        return null;
-                } else {
-                    i = 5;
-                    alert ('Texto digitado não encontrado!');
+                if(lista2[i] == retorno){  
+                    return null;
+
+                } else {            
+                    break;
                 }  
             }
         }
-                
-       while(controle != 0){
+        function fluxoFrontEnd(){
+            controle3 = 1;
+            while (controle3 != 0){
+            
+             
+            controle3 = prompt('Dentro da carreira de Front-End, escolha: React ou Vue? Caso queira sair digite 0')
+                    if(controle3 == 0){
+                        break;
+                    }
+
+            retornoFluxoFront = controle3.toLowerCase();
+            if (verificaRetorno(retornoFluxoFront,respostasFluxoFront1,respostasFluxoFront2) || retornoFluxoFront != tecnologiasAprendidas){
+               
+                alert('Parabêns!                               Você aprendeu React!')
+                    controle3 =- 1;
+                        tecnologiasAprendidas.push('React');
+                            controle3 = prompt('Deseja aprender outra teconologia em Front-End? Caso queira sair digite 0')
+                                if(verificaRetorno(controle3,respostasNegativas,respostasPositivas || controle3 == 0)){
+                                    controle3 = 0;
+                                    break;
+                            }   
+            } else {
+                if (verificaRetorno(retornoFluxoFront,respostasFluxoFront1,respostasFluxoFront2) == null || retornoFluxoFront != tecnologiasAprendidas ){
+                alert('Parabens!                                 Você aprendeu Vue !')
+                    controle3 =- 1;
+                        tecnologiasAprendidas.push('Vue');
+                            controle3 = prompt('Deseja aprender outra teconologia em Front-End? Caso queira sair digite 0')
+                               if(verificaRetorno(controle3,respostasNegativas,respostasPositivas || controle3 == 0)){
+                                    controle3 = 0;
+                                    break;
+                            }
+                } else {
+                    if (retornoFluxoFront != tecnologiasAprendidas){
+                        alert('Tecnologia já estudada!')
+                    } else {
+                        alert ('Texto digitado não cadastrado!');
+                    }      
+                 }
+            }               
+            }
+        }
+        
+        function fluxoBackEnd(){
+            while (controle != 0){
+            controle = prompt('Dentro da carreira de Front-End, escolha: React ou Vue? ')
+                    if(controle == 0){
+                        break;
+                    }
+            retornoFluxoFront = controle.toLowerCase();
+            if (verificaRetorno(retornoFluxoFront,respostasFluxoFront1,respostasFluxoFront2)){
+                alert('Pabens!Você aprendeu Vue!')
+                tecnologiasAprendidas.push('Vue')
+                controle = prompt('Deseja aprender outra teconologia em Front-End? Caso queira sair digite 0')
+            } else {
+                alert('Parabens! Você aprendeu React!')
+                tecnologiasAprendidas.push('React')
+                controle = prompt('Deseja aprender outra teconologia em Front-End? Caso queira sair digite 0')
+            }                
+
+            } 
+        } 
+
+        while(controle != 0){
            controle2 = 1;
 
             while (controle2 != 0){
                 controle = prompt('Escolha... Front-End ou Back-End ?                      Caso queria encerrar , digite 0')
-                    if (controle = 0){
+                    if(controle == 0){
                         break;
                     }
+
                 retornoDoUsuario = controle.toLowerCase();
 
             if(verificaRetorno(retornoDoUsuario,respostasParaFront,respostasParaBack)){
-                    fluxoFrontEnd();
+                carreira.push('Front-End')
+                fluxoFrontEnd();
             } else {
-                if (verificaRetorno(retornoDoUsuario,respostasParaFront,respostasParaBack) === null){
-                    fluxoBackEnd();
+                
+            if (verificaRetorno(retornoDoUsuario,respostasParaFront,respostasParaBack) === null){
+                carreira.push('Back-End')
+                fluxoBackEnd();   
+                
             } else {
+                    alert('Resposta ainda não cadastrada!');
                     controle2 = 0;
             }
             }
             }
         }
-
