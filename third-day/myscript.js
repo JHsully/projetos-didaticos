88f90ae385a762c39848d31561c45add5153b261
John H. Moreira <98336461+JHsully@users.noreply.github.com>
2022-08-05 00:05:51 -0300

    Update da finalização do projeto.
    Correção de bugs e finalização das funções.

--- third-day/myscript.js
+++ third-day/myscript.js
@@ -2,47 +2,136 @@
 
         let controle = 1;
         let controle2 = 1;
-        let controle3 = 1;
+        let controle3 = 2;
 
-        //tamanho das duas listas deverá ser igual para que a função verificaRetorno não de erro.
+        let carreira = [];    
+        let retornoDoUsuario;
+        let retornoFluxoFront;
+        let retornoFluxoBack;
+        let tecnologiasAprendidas = [];
+        let listaBase = [];
+
+          //tamanho das listas semelhantes deverá ser igual para que a função que verifica o retorno não de erro.
         let respostasParaFront = ['front','front-end','frontend']
         let respostasParaBack = ['back','back-end','backend',]
-
+        let respostasFluxoFront1 = ['react','rect','ract']
+        let respostasFluxoFront2 = ['vue','vuue','vrue']
+        let respostasFluxoBack1 = ['c#','c']
+        let respostasFluxoBack2 = ['java','jaava']
+        let respostasPositivas = ['sim','ok','pode ser','quero',]
+        let respostasNegativas = ['nao','não','nao quero','nao desejo',]
 
         function verificaRetorno (retorno,lista1,lista2){
 
-            for(i = 0; i < respostasParaBack.length; i++){
-                if(respostasParaFront[i] == retornoDoUsuario){
-                        return true;
+            for(i = 0; i < lista1.length; i++){
+                if(lista1[i] == retorno){
+                    return true
+                        break;
                 }  
-                if(respostasParaBack[i] == retornoDoUsuario){
-                        return null;
-                } else {
-                    i = 5;
-                    alert ('Texto digitado não encontrado!');
-                }  
+                if(lista2[i] == retorno){  
+                    return null
+                        break;
+
+                }   
             }
         }
-                
-       while(controle != 0){
-           controle2 = 1;
 
-            while (controle2 != 0){
-                controle = prompt('Escolha... Front-End ou Back-End ?                      Caso queria encerrar , digite 0')
-                    if (controle = 0){
+
+        function fluxoFrontEnd(){
+            controle3 = 1;
+            while (controle3 != 0){
+                controle3 = prompt('Dentro da carreira de Front-End, escolha: React ou Vue? \n Caso queira sair digite 0')
+                    if(controle3 == 0){
                         break;
                     }
-                retornoDoUsuario = controle.toLowerCase();
+                retornoFluxoFront = controle3.toLowerCase();
+                     if (verificaRetorno(retornoFluxoFront,respostasFluxoFront1,listaBase)  && verificaRetorno(retornoFluxoFront,tecnologiasAprendidas,listaBase) != true){
+                        alert('Parabêns!   Você aprendeu: \n React!')
+                            tecnologiasAprendidas.push('react');
+                                                
+                    } else {
+                        if(verificaRetorno(retornoFluxoFront,respostasFluxoFront2,listaBase)  && verificaRetorno(retornoFluxoFront,tecnologiasAprendidas,listaBase) != true){
+                            alert('Parabens! Você aprendeu: \n Vue !')
+                                tecnologiasAprendidas.push('vue');
+                                            
+                    } else {
+                        if(verificaRetorno(retornoFluxoFront,tecnologiasAprendidas,listaBase) == true){
+                            alert('Tecnologia já aprendida!')
+                    } else {
+                            alert('Tecnologia não encontrada');
+                    }
+                    }  
+                    }      
+            }
+            }
 
-            if(verificaRetorno(retornoDoUsuario,respostasParaFront,respostasParaBack)){
+            function fluxoBackEnd(){
+                controle3 = 1;
+                while (controle3 != 0){
+                    controle3 = prompt('Dentro da carreira de Back-End, escolha: C# ou Java ? \n Caso queira sair digite 0')
+                        if(controle3 == 0){
+                            break;
+                        }
+                    retornoFluxoBack = controle3.toLowerCase();
+                         if (verificaRetorno(retornoFluxoBack,respostasFluxoBack1,listaBase)  && verificaRetorno(retornoFluxoBack,tecnologiasAprendidas,listaBase) != true){
+                            alert('Parabêns!   Você aprendeu: \n C# !')
+                                tecnologiasAprendidas.push('c#');
+                                                    
+                        } else {
+                            if(verificaRetorno(retornoFluxoBack,respostasFluxoBack2,listaBase)  && verificaRetorno(retornoFluxoBack,tecnologiasAprendidas,listaBase) != true){
+                                alert('Parabens! Você aprendeu: \n Java !')
+                                    tecnologiasAprendidas.push('java');
+                                                     
+                        } else {
+                            if(verificaRetorno(retornoFluxoBack,tecnologiasAprendidas,listaBase) == true){
+                                alert('Tecnologia já aprendida!')
+                        } else {
+                                alert('Tecnologia não encontrada');
+                        }
+                        }  
+                        }      
+                }
+                }
+
+        while(controle != 0){
+           controle2 = 1;   
+                if ( (tecnologiasAprendidas.length) > 0){  
+                    controle = prompt('Deseja aprender outra tecnologia?')
+                        if (verificaRetorno(controle,respostasPositivas,respostasNegativas) == true){
+                            controle = prompt('Digite a tecnologia...')
+                                tecnologiasAprendidas.push(controle);
+                                 alert('Você aprendeu '+ controle +'!')
+                        } else {
+                            if (verificaRetorno(controle,respostasPositivas,respostasNegativas) === null && (tecnologiasAprendidas.length) > 0){
+                                alert('Você Aprendeu: \n'+tecnologiasAprendidas+'\nParabêns!\nPrograma encerrado.');
+                                    break;
+                        } else {
+                            if (controle != 1){
+                                alert('Texto digitado não encontrado.')
+                            }
+                        }  
+                        }
+                }  
+    
+            while (controle2 != 0){
+                controle2 = prompt('Escolha... Front-End ou Back-End ?\n Caso queria encerrar e ver as tecnologias aprendidas, digite 0')
+                    if(controle2 == 0) {
+                        break;
+                    }
+
+                retornoDoUsuario = controle2.toLowerCase();
+
+                if(verificaRetorno(retornoDoUsuario,respostasParaFront,respostasParaBack)){
                     fluxoFrontEnd();
-            } else {
-                if (verificaRetorno(retornoDoUsuario,respostasParaFront,respostasParaBack) === null){
-                    fluxoBackEnd();
-            } else {
-                    controle2 = 0;
-            }
-            }
+                
+                } else {
+                    if (verificaRetorno(retornoDoUsuario,respostasParaFront,respostasParaBack) === null){
+                        fluxoBackEnd();   
+                
+                    } else {
+                            alert('Resposta ainda não cadastrada!');
+                                controle2 = 0;
+                    }
+                }
             }
         }
-
